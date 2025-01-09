b855ed6f7e59b7515678d9cc788d33967aa9485d
andiez-dungpv <pvdung@humaxdigital.com>
2025-01-09 18:11:35 +0700

    first commit

---
+++ present-next-14/src/app/images/page.js
@@ -0,0 +1,13 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  return (
+    <div className="image_list">
+      <img src="https://c4.wallpaperflare.com/wallpaper/872/714/351/bayern-munich-arjen-robben-wallpaper-preview.jpg" loading="lazy" />
+      <img src="https://wallpapercave.com/wp/wp1869993.jpg" loading="lazy" />
+      <img src="https://i.pinimg.com/originals/c9/e0/fb/c9e0fb33d209cc5304549024bba993e9.jpg" loading="lazy" />
+      <img src="https://img.fcbayern.com/image/upload/t_cms-1x1-seo/v1601358822/cms/public/images/fcbayern-com/homepage/saison-16-17/profis/robben/robben_ima_200513.jpg" loading="lazy" />
+    </div>
+  );
+}
